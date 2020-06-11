7188b63317212f88610e658c507ef5b5453371ec
margarita-sh <margaritka1992@gmail.com>
2020-06-11 19:38:28 +0300

    fixed a bug

--- src/app/store/selectors/api.selectors.ts
+++ src/app/store/selectors/api.selectors.ts
@@ -4,6 +4,6 @@ import { TranslateState, featureKeyTranslate } from '../state/translate.state';
 export const selectStateTranslate: any = createFeatureSelector<TranslateState>(featureKeyTranslate);
 
 // tslint:disable-next-line: max-line-length
-export const selectWordEng: any = createSelector(selectStateTranslate, (state: TranslateState) => console.log('state.wordEng', state.wordEng));
+export const selectWordEng: any = createSelector(selectStateTranslate, (state: TranslateState) => state.wordEng);
 // tslint:disable-next-line: max-line-length
-export const selectWordRu: any = createSelector(selectStateTranslate, (state: TranslateState) => console.log('state.wordEng', state.wordRu));
+export const selectWordRu: any = createSelector(selectStateTranslate, (state: TranslateState) => state.wordRu);
