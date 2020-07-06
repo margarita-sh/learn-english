e841a2eb35841b2f6c4344a1e561928b0ed5b9b0
margarita-sh <margaritka1992@gmail.com>
2020-07-06 13:59:01 +0300

    done

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss']
 })
-export class AboutMeComponent implements OnInit {
+export class AboutMeComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private translate: TranslateService) {
+	translate.addLangs(['en', 'ru']);
+	translate.setDefaultLang('en');
+	const browserLang: any = translate.getBrowserLang();
+	translate.use(browserLang.match(/en|ru/) ? browserLang : 'en');
   }
 
 }
