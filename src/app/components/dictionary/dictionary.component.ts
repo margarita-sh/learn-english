baddbfdf303d5ef43e4b38d6bf3d124923728938
margarita-sh <margaritka1992@gmail.com>
2020-07-06 13:25:59 +0300

    correct dictionary

--- src/app/components/dictionary/dictionary.component.ts
+++ src/app/components/dictionary/dictionary.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { DataGameService } from '../game/service/data-game.service';
 import { Word } from '../game/word.model';
 import { AudioService } from '../game/service/audio.service';
@@ -8,8 +8,8 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { Store, select } from '@ngrx/store';
 import { DictionaryState } from 'src/app/store/state/dictionary.state';
-import { removeWordFromDictionary, changeWordStatus, getAudioSrc } from 'src/app/store/action/dictionary.action';
-import { Observable } from 'rxjs';
+import { removeWordFromDictionary, changeWordStatus, getAudioSrc, setAudioSrc } from 'src/app/store/action/dictionary.action';
+import { Observable, Subscription } from 'rxjs';
 import { selectDictionary, selectSrcAudio } from 'src/app/store/selectors/dictionary.selectors';
 import { Dictionary } from './dictionary.model';
 
@@ -18,7 +18,9 @@ import { Dictionary } from './dictionary.model';
 	templateUrl: './dictionary.component.html',
 	styleUrls: ['./dictionary.component.scss']
 })
-export class DictionaryComponent implements OnInit {
+export class DictionaryComponent implements OnInit, OnDestroy {
+	private dictionarySubscription: Subscription;
+	private srcAudioSubscription: Subscription;
 	public dictionary$: Observable<Word[]> = this._store$.pipe(select(selectDictionary));
 	public srcAudio$: Observable<object> = this._store$.pipe(select(selectSrcAudio));
 	public audio: HTMLAudioElement;
@@ -49,13 +51,13 @@ export class DictionaryComponent implements OnInit {
 	}
 
 	public ngOnInit(): void {
-		this.dictionary$.subscribe((data: Word[]) => {
+		this._store$.dispatch(setAudioSrc({ word: new Word(), src: '' }));
+		this.dictionarySubscription = this.dictionary$.subscribe((data: Word[]) => {
 			this.dataSource.data = data;
 			this.dataSource.paginator = this.paginator;
 		});
-		console.log('this.dataSource', this.dataSource.data);
 
-		this.srcAudio$.subscribe((data: Dictionary) => {
+		this.srcAudioSubscription = this.srcAudio$.subscribe((data: Dictionary) => {
 			// tslint:disable-next-line: strict-boolean-expressions
 			if (!data.src) {
 				return;
@@ -64,8 +66,12 @@ export class DictionaryComponent implements OnInit {
 			this.audio.play();
 			this.spinnerButtonOptions.active = false;
 			this._store$.dispatch(changeWordStatus({ word: data.word, isLoading: false }));
-
 		});
+	}
+
+	public ngOnDestroy(): void {
+		this.dictionarySubscription.unsubscribe();
+		this.srcAudioSubscription.unsubscribe();
 	}
 
 	public removeWordFromDictionary(word: Word): void {
